5fb7cd8663865e3995e4e4179a82d54bb015adf7
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-04-19 22:13:05 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,32 +3,32 @@ import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="footer items-center p-4 bg-neutral/70 text-neutral-content">
+    <footer className="footer items-center p-2 bg-neutral/70 text-neutral-content">
       <aside className="items-center grid-flow-col text-xs md:text-sm mx-auto md:mx-0">
         <p>Copyright&copy;2024 - All right reserved by Pokrov.</p>
       </aside>
-      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end mx-auto md:mx-0">
+      <nav className="grid-flow-col md:place-self-center md:justify-self-end mx-auto md:mx-0">
         <Link
           href="https://facebook.com/pokrov.ltd/"
           target="_blank"
           aria-label="Посилання на сторінку Facebook"
-          className="flex justify-center items-center p-2 border-2 rounded-md border-gray-500 hover:border-primary hover:text-primary"
+          className="btn btn-ghost"
         >
           <FaFacebookF className="w-5 h-5" />
         </Link>
         <Link
           href="https://www.instagram.com/"
           target="_blank"
-          aria-label="Посилання на сторінку у Instagram"
-          className="flex justify-center items-center p-2 border-2 rounded-md border-gray-500 hover:border-primary hover:text-primary"
+          aria-label="Посилання на сторінку Instagram"
+          className="btn btn-ghost"
         >
           <FaInstagram className="w-5 h-5" />
         </Link>
         <Link
           href="https://www.linkedin.com/"
           target="_blank"
-          aria-label="Посилання на сторінку у LinkedinIn"
-          className="flex justify-center items-center p-2 border-2 rounded-md border-gray-500 hover:border-primary hover:text-primary"
+          aria-label="Посилання на сторінку LinkedinIn"
+          className="btn btn-ghost"
         >
           <FaLinkedinIn className="w-5 h-5" />
         </Link>
